48bd735ec6c3cbf7b564780d7a4f4bf605b87cd9
Dakota Schramm <william.schramm11@gmail.com>
2023-07-26 22:14:42 -0500

    Add smoke test redis.

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+
+import { client as redis } from '~/utils/redis'
+
+export  async function GET() {
+  await redis.connect()
+
+  redis.set('foo', 'bar')
+
+  const value = await redis.get('foo');
+  console.log(value) // returns 'bar'
+
+  await redis.disconnect();
+  return NextResponse.json({ data: value })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 'use client';
 
+import { useState, useEffect } from 'react';
+
 import useRedis from '~/hooks/useRedis';
 
 export default function Home() {
   const data = useRedis();
+  const [redisHello, setReidsHello] = useState('');
+
+  useEffect(() => {
+    async function fetchRedisHello() {
+      const res = await fetch('/api/hello');
+      const msg = await res.json();
+      setReidsHello(msg.data);
+    }
+    fetchRedisHello();
+  }, [])
+  
 
   return (
     <main>
@@ -14,6 +27,7 @@ export default function Home() {
           ?? <li>loading...</li>
         }
       </ul>
+      <p>{redisHello}</p>
     </main>
   )
 }
